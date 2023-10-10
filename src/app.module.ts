19573597e628a56d2b622ff5391e7b4d9014458c
maxmaxph <maxmaxph@gmail.com>
2023-10-10 13:38:12 +0200

    3-configuration prefixe des routes, valisationpipe, swagger & cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Importe le module de configuration pour gérer les variables d'environnement
+    ConfigModule.forRoot({ envFilePath: [`.env`] }),
+
+    // Importe le module TypeORM pour la connexion à la base de données
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Spécifie le type de base de données (PostgreSQL)
+      host: 'localhost', // Adresse de l'hôte de la base de données
+      port: +process.env.POSTGRES_PORT, // Port de la base de données (converti en nombre)
+      username: process.env.POSTGRES_USER, // Nom d'utilisateur pour la connexion à la base de données
+      password: process.env.POSTGRES_PASSWORD, // Mot de passe pour la connexion à la base de données
+      database: process.env.POSTGRES_DATABASE, // Nom de la base de données
+      synchronize: false, // Désactive la synchronisation automatique du schéma de la base de données
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Définit un préfixe global pour toutes les routes de l'API
+  app.setGlobalPrefix(`api`);
+
+  // Utilise un pipe global pour valider les données entrantes selon les décorateurs de classe DTO
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Active CORS pour permettre aux requêtes de différents domaines d'accéder à l'API
+  app.enableCors();
+
+  // Configuration module Swagger (doc API)
+  const config = new DocumentBuilder()
+    .setTitle('RAIL ZEN API') // Titre de la documentation
+    .setDescription('Meditation pour les cheminots de la SNCF') // Description de l'API
+    .setVersion('1.0') // Version de l'API
+    .addTag('railzen') // Ajoute une étiquette pour regrouper les routes/endpoints
+    .build();
+
+  // Crée un document Swagger basé sur les configurations et l'application NestJS
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Configure l'application pour utiliser Swagger sur le chemin '/api'
+  SwaggerModule.setup('api', app, document);
+
+  // Démarre l'application pour écouter sur le port 3000
   await app.listen(3000);
 }
+
 bootstrap();
